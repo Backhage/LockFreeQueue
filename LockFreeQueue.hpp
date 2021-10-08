16bbcddc189b0562cb23f3359a736e784addef66
Eric Backhage <eric.backhage@gmail.com>
2021-10-08 23:47:45 +0200

    Initial commit

---
+++ LockFreeQueue.hpp
@@ -0,0 +1,94 @@
+/* A queue that is thread safe without using a mutex */
+#pragma once
+
+#include <atomic>
+#include <cstdlib>
+#include <optional>
+#include <type_traits>
+
+template<typename ElemT, std::size_t MaxSize>
+class LockFreeQueue {
+    using Index = decltype(MaxSize);
+    struct Indices {
+        Index readIndex;
+        Index writeIndex;
+    };
+
+    using Size = decltype(MaxSize);
+    static constexpr Size BufferSize = MaxSize + 1;
+
+    static constexpr Index nextIndex(Index currentIndex) {
+        return currentIndex < MaxSize ? currentIndex + 1 : 0;
+    }
+
+    using Elem = ElemT;
+    Elem* getElemByIndex(Index index) {
+        return reinterpret_cast<Elem*>(&m_buffer[index]);
+    }
+
+    std::atomic<Indices> m_indices{};
+    std::atomic<bool> m_occupied[BufferSize]{};
+    typename std::aligned_storage_t<sizeof(Elem), alignof(Elem)> m_buffer[BufferSize];
+
+public:
+    LockFreeQueue() = default;
+    ~LockFreeQueue() {
+        Indices currentIndices = m_indices ;
+        while (currentIndices.readIndex != currentIndices.writeIndex) {
+            if (m_occupied[currentIndices.readIndex]) {
+                getElemByIndex(currentIndices.readIndex)->~Elem();
+                m_occupied[currentIndices.readIndex] = false;
+                currentIndices.readIndex = nextIndex(currentIndices.readIndex);
+            }
+        }
+    }
+    LockFreeQueue(const LockFreeQueue&) = delete;
+    LockFreeQueue& operator=(const LockFreeQueue&) = delete;
+
+    /** 
+     * Puts a new element into the queue. 
+     * Input: Constructor arguments for the new element to place in queue
+     * Return: true if element was successfully placed in queue, otherwise
+     *         false.
+    */
+    template<typename... ArgTs>
+    bool tryPut(ArgTs&&... args) {
+        Indices currentIndices = m_indices;
+        auto nextWriteIndex{ nextIndex(currentIndices.writeIndex) };
+
+        if (nextWriteIndex == currentIndices.readIndex) {
+            return false;
+        }
+
+        if (!m_indices.compare_exchange_strong(currentIndices, {currentIndices.readIndex, nextWriteIndex})) {
+            return false;
+        }
+
+        new(&m_buffer[currentIndices.writeIndex]) Elem{std::forward<ArgTs>(args)...};
+        m_occupied[currentIndices.writeIndex] = true;
+        return true;
+    }
+
+    /** 
+     * Removes and returns the element at the front of the queue.  
+     * Return: The element wrapped in std::optional if sucessful, otherwise
+     *         std::nullopt
+     */
+    std::optional<Elem> tryPop() {
+        Indices currentIndices = m_indices;
+        if (currentIndices.readIndex == currentIndices.writeIndex) {
+            return std::nullopt;
+        }
+
+        auto nextReadIndex{ nextIndex(currentIndices.readIndex) };
+        if (!m_indices.compare_exchange_strong(currentIndices, {nextReadIndex, currentIndices.writeIndex})) {
+            return std::nullopt;
+        }
+        static_assert(std::is_copy_constructible_v<Elem>);
+        Elem elem{ *getElemByIndex(currentIndices.readIndex) };
+        static_assert(std::is_nothrow_destructible_v<Elem>);
+        getElemByIndex(currentIndices.readIndex)->~Elem();
+        m_occupied[currentIndices.readIndex] = false;
+        return elem;
+    }
+};
---
+++ LockFreeQueue_Test.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+#include "LockFreeQueue.hpp"
+
+TEST(LockFreeQueueTest, PushAndPop) {
+  LockFreeQueue<int, 1> q;
+  constexpr int expected = 1;
+  ASSERT_TRUE(q.tryPut(expected));
+
+  auto actual = q.tryPop();
+  ASSERT_TRUE(actual.has_value());
+  ASSERT_EQ(*actual, expected);
+}
+
+TEST(LockFreeQueue, MemLeakCheck) {
+  // Run with 'valgrind --leak-check=full' to detect memory leaks
+  class RAII {
+    const int m_size = 10;
+    int* m_data;
+
+  public:
+    RAII() : m_data{ new int[m_size] } {
+    }
+    ~RAII() {
+      delete [] m_data;
+    }
+
+    RAII(const RAII& other) : m_data{ new int[m_size] } {
+      // Don't care about deep copy of data.
+    }
+  };
+
+  LockFreeQueue<RAII, 2> q;
+  ASSERT_TRUE(q.tryPut());
+  ASSERT_TRUE(q.tryPut());
+
+  auto fst = q.tryPop();
+  ASSERT_TRUE(fst.has_value());
+}
